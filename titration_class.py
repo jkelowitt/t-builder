eb098597d0753d4b6215b27c17327642b73b55a2
Jackson Elowitt <jkelowitt@protonmail.com>
2020-12-22 21:49:12 -0800

    Added doc-strings and additional customizability to various functions.

--- titration_class.py
+++ titration_class.py
@@ -76,38 +76,65 @@ class Titration:
 
     @staticmethod
     def pk_to_k(pk):
-        """Converts a pk, or an array or pk's to a k or an array of k's"""
+        """
+        Converts a pk, or an array or pk's to a k or an array of k's
+
+        :param pk:
+            A or a list of pK values for a compound.
+        :return:
+            An array of the pk values respective k values in order.
+
+        """
         k = np.array(10. ** (- np.array(pk)))
         return k
 
     def check_values(self):
-        """Find only the useful data."""
+        """
+        Returns the class data trimmed to only be within a certain titration completion range.
+        If a phi value of 1 represents a volume 100% of the way to the equivalence point, then a value of 2 should show
+            the whole equivalence point and the end of the curve. Thus, the range we wish to show should always have a
+            phi value of one more than the number of equivalence points.
+
+        :return:
+        volume_titrant, ph, hydronium, hydroxide, alpha_titrant, alpha_analyte, all trimmed to be within the
+            specified range.
+        """
 
         # Go until you are 1 past the last sub-reaction.
         limiter = len(self.pka_values) + 1
 
-        good_val_index = np.where((self.volume_titrant >= 0) & (self.phi <= limiter))
+        good_val_index = np.where((self.phi >= 0) & (self.phi <= limiter))
 
         # Cut the bad data out of each dataset.
-        self.volume_titrant = self.volume_titrant[good_val_index]
-        self.ph = self.ph[good_val_index]
-        self.hydronium = self.hydronium[good_val_index]
-        self.hydroxide = self.hydroxide[good_val_index]
+        volume_titrant = self.volume_titrant[good_val_index]
+        ph = self.ph[good_val_index]
+        hydronium = self.hydronium[good_val_index]
+        hydroxide = self.hydroxide[good_val_index]
 
         # Trim the alpha_analyte values
         new_alphas = []
         for alphas in self.alpha_analyte.T:
             new_alphas.append(alphas[good_val_index])
-        self.alpha_analyte = np.array(new_alphas).T
+        alpha_analyte = np.array(new_alphas).T
 
         # Trim the alpha_titrant values
         new_alphas = []
         for alphas in self.alpha_titrant.T:
             new_alphas.append(alphas[good_val_index])
-        self.alpha_titrant = np.array(new_alphas).T
+        alpha_titrant = np.array(new_alphas).T
+
+        return volume_titrant, ph, hydronium, hydroxide, alpha_titrant, alpha_analyte
 
     def starting_phs(self, min_ph=0, max_ph=14):
-        """Generate pH, [H+], and [OH-] lists"""
+        """
+        Creates a starting range of pH, hydronium, and hydroxide values.
+        :param min_ph:
+            The minimum value of pH to calculate from.
+        :param max_ph:
+            The maximum value of pH to calculate from.
+        :return: ph, h, oh
+            pH, hydronium concentration, and hydroxide concentration.
+        """
         # pH
         ph = np.array(np.arange(min_ph, max_ph + self.precision, step=self.precision))
 
@@ -118,7 +145,13 @@ class Titration:
 
     @staticmethod
     def scale_alphas(arr):
-        # TODO make this less horrible
+        """
+        Scales each sub array by its index in the parent array.
+        :param arr:
+            Array to be scaled
+        :return: new_arr
+            Array which has been scaled.
+        """
         new_arr = []
         for item in arr:
             sub_arr = []
@@ -131,6 +164,17 @@ class Titration:
         return new_arr
 
     def alpha_values(self, k, h, acid=True):
+        """
+        Calculates the alpha values for the given compound.
+        :param k:
+            A list of k values for the
+        :param h:
+            A list of hydronium concentrations.
+        :param acid:
+            Whether or not the compound is acidic.
+        :return:
+            A numpy array of alpha values for the compound.
+        """
 
         # Convert lists to numpy arrays for easier math
         h = np.array(h)
@@ -144,14 +188,14 @@ class Titration:
         n = len(k)
 
         # Get the values for the [H+]^n power
-        powers = [x for x in range(n, -1, -1)]  # List of powers
-        h_vals = [np.array(h ** i) for i in powers]  # List of H values raised to the powers
+        powers = np.array([x for x in range(n, -1, -1)])  # List of powers
+        h_vals = np.array([np.array(h ** i) for i in powers])  # List of H values raised to the powers
 
-        # Get the scalar factors which depend solely on the k values
-        k_vals = [np.prod(k[0:x]) if k.size > 0 else [1] for x in range(n + 1)]
+        # Get the products of the k values.
+        k_vals = [np.prod(k[0:x]) for x in range(n + 1)]
 
         # Prod and Sum the h and k values
-        h_vals = np.transpose(h_vals)  # Reorient the array for multiplication
+        h_vals = h_vals.T  # Reorient the array for multiplication
         denoms_arr = np.multiply(h_vals, k_vals)  # Product of the sub-elements of the denominator
         denoms = np.sum(denoms_arr, axis=1)  # Sum of the sub-elements of the denominator
 
@@ -166,6 +210,13 @@ class Titration:
         return np.array(alphas)
 
     def volume_calculator(self, acid_titrant):
+        """
+        Calculates the volume of titrant for each pH for the titrant and analyte.
+        :param acid_titrant:
+            Whether or not the titrant is an acid.
+        :return: volume, phi
+            The volume and reaction progress.
+        """
 
         # Alpha values scaled by their index
         scaled_alphas_analyte = self.scale_alphas(self.alpha_analyte)
@@ -198,7 +249,14 @@ class Titration:
         volume = phi * self.volume_analyte * self.concentration_analyte / self.concentration_titrant
         return volume, phi
 
-    def plot_titration_curve(self):
+    def plot_titration_curve(self, title):
+        """
+        Plots a titration curve for the current analyte and titrant at the trimmed pH values
+        :param title:
+            Title for the plot.
+        :return:
+            None
+        """
         # Remove values from indices where the volume is negative or extremely large.
         self.check_values()
 
@@ -206,28 +264,61 @@ class Titration:
         plt.title(title)
         plt.show()
 
-    def plot_alpha_curve(self):
-
+    def plot_alpha_curve(self, title):
+        """
+        Plots a bjerrum curve for the current analyte at all pH values.
+        :param title:
+            Title for the plot.
+        :return:
+            None
+        """
         plt.plot(self.ph, self.alpha_analyte)
-        plt.title("Alpha Values for\n" + self.title)
+        plt.title(title)
         plt.show()
 
-    def write_titration_data(self):
-        # Remove values from indices where the volume is negative or extremely large.
-        self.check_values()
+    def write_titration_data(self, title, file_headers=False):
+        """
+        Write to a csv file, the volume of titrant and pH at each addition.
+        :param title:
+            Title for the csv
+        :param file_headers:
+            Whether or not to include headers in the csv file.
+        :return:
+            None
+        """
+        # Make dataframe.
+        data = pd.DataFrame({"volume": self.volume_titrant_t,
+                             "pH": self.ph_t})
 
-        # Make and write the data frame to a csv
-        data = pd.DataFrame({"volume": self.volume_titrant, "pH": self.ph})
-        data.to_csv(f"{self.title} titration data.csv", index=False)
+        # Write to a csv.
+        data.to_csv(f"{title}.csv", index=False, header=file_headers)
 
-    def write_alpha_data(self):
+    def write_alpha_data(self, title, file_headers=False, species_names=None):
+        """
+        Write to a csv file, the pH and ampholyte prevalence for each species in solution.
+        :param title:
+            Title for the csv
+        :param file_headers:
+            Whether or not to include headers in the csv file.
+        :param species_names:
+            Ordered names for each of the ampholyte species.
+        :return:
+            None
+        """
         # Initialize the dataframe with the ph values
         data_dict = {"pH": self.ph}
 
         # Add the alpha values for each analyte species
-        for num, alpha in enumerate(self.alpha_analyte.T):
-            data_dict[f"alpha{num}"] = alpha
+        if species_names is None:  # If names are not specified, just use generics.
+            for num, alpha in enumerate(self.alpha_analyte.T):
+                data_dict[f"alpha{num}"] = alpha
+        else:  # If names are specified, use them.
+            for num, alpha in enumerate(self.alpha_analyte.T):
+                try:
+                    data_dict[species_names[num]] = alpha
+                except IndexError:
+                    raise ValueError("You have not supplied enough species names!")
 
         # Make and write the data frame to a csv
         data = pd.DataFrame(data_dict)
-        data.to_csv(f"{self.title} alpha data.csv", index=False)
+        data.to_csv(f"{title}.csv", index=False, header=file_headers)
