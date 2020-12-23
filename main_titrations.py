7685650a3b51075cc2cff251bc115a45cc980f24
Jackson Elowitt <jkelowitt@protonmail.com>
2020-12-22 16:07:10 -0800

    Updated to have correct analyte/titrant strengths.
    Changed strong pKa and pKt values to -6. This is correct for a strong acid/base. ~= to the pKa for HCl
    
    Put objects into a list and called the plot function on each. This made it easier to change the plotting function being run.
    
    Reordered the objects to be sorted by analyte type, acidic or basic.
    
    There are now 6 of each analyte titrations.

--- main_titrations.py
+++ main_titrations.py
@@ -1,122 +1,154 @@
 from titration_class import Titration
 
+"""Acid Analytes"""
 # Strong Monoprotic Acidic Analyte, Strong Monoprotic Basic Titrant.
 a = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
-              pka_values=[1000],
-              pkt_values=[1000],
+              pka_values=[-6],
+              pkt_values=[-6],
+              strong_analyte=True,
+              strong_titrant=True,
               title="Strong Monoprotic Acidic Analyte, Strong Monoprotic Basic Titrant")
-a.plot_titration_curve()
 
 # Weak Monoprotic Acidic Analyte, Strong Monoprotic Basic Titrant.
-a = Titration(analyte_is_acidic=True,
+b = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
               pka_values=[6],
-              pkt_values=[1000],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
               title="Weak Monoprotic Acidic Analyte, Strong Monoprotic Basic Titrant.")
-a.plot_titration_curve()
 
 # Weak Diprotic Acidic Analyte, Strong Monoprotic Basic Titrant.
-a = Titration(analyte_is_acidic=True,
+c = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
               pka_values=[3, 9],
-              pkt_values=[1000],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
               title="Weak Diprotic Acidic Analyte, Strong Monoprotic Basic Titrant.")
-a.plot_titration_curve()
 
 # Weak Triprotic Acidic Analyte, Strong Monoprotic Basic Titrant.
-a = Titration(analyte_is_acidic=True,
+d = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
               pka_values=[3, 6, 9],
-              pkt_values=[1000],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
               title="Weak Triprotic Acidic Analyte, Strong Monoprotic Basic Titrant.")
-a.plot_titration_curve()
-
-# Strong Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
-a = Titration(analyte_is_acidic=False,
-              titrant_is_acidic=True,
-              volume_analyte=25,
-              concentration_analyte=0.10,
-              concentration_titrant=0.10,
-              pka_values=[1000],
-              pkt_values=[1000],
-              title="Strong Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
-a.plot_titration_curve()
-
-# Weak Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
-a = Titration(analyte_is_acidic=False,
-              titrant_is_acidic=True,
-              volume_analyte=25,
-              concentration_analyte=0.10,
-              concentration_titrant=0.10,
-              pka_values=[6],
-              pkt_values=[1000],
-              title="Weak Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
-a.plot_titration_curve()
-
-# Weak Difunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
-a = Titration(analyte_is_acidic=False,
-              titrant_is_acidic=True,
-              volume_analyte=25,
-              concentration_analyte=0.10,
-              concentration_titrant=0.10,
-              pka_values=[3, 9],
-              pkt_values=[1000],
-              title="Weak Difunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
-a.plot_titration_curve()
-
-# Weak Trifunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
-a = Titration(analyte_is_acidic=False,
-              titrant_is_acidic=True,
-              volume_analyte=25,
-              concentration_analyte=0.10,
-              concentration_titrant=0.10,
-              pka_values=[3, 6, 9],
-              pkt_values=[1000],
-              title="Weak Trifunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
-a.plot_titration_curve()
-
-# Weak Monofunctional Basic Analyte, Weak Monoprotic Acidic Titrant.
-a = Titration(analyte_is_acidic=False,
-              titrant_is_acidic=True,
-              volume_analyte=25,
-              concentration_analyte=0.10,
-              concentration_titrant=0.10,
-              pka_values=[7],
-              pkt_values=[5],
-              title="Weak Monofunctional Basic Analyte, Weak Monoprotic Acidic Titrant.")
-a.plot_titration_curve()
 
 # Weak Monoprotic Acidic Analyte, Weak Monofunctional Basic Titrant.
-a = Titration(analyte_is_acidic=True,
+e = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
               pka_values=[7],
               pkt_values=[5],
+              strong_analyte=False,
+              strong_titrant=True,
               title="Weak Monoprotic Acidic Analyte, Weak Monofunctional Basic Titrant.")
-a.plot_titration_curve()
 
 # Weak Polyprotic Acidic Analyte, Weak Polyfunctional Basic Titrant.
-a = Titration(analyte_is_acidic=True,
+f = Titration(analyte_is_acidic=True,
               titrant_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
               pka_values=[3, 4.5, 5, 6, 7.23, 8, 9, 10, 11, 12],
               pkt_values=[3, 4.5, 5, 6, 7, 8, 9, 10, 11, 12],
-              title=" Weak Polyprotic Acidic Analyte, Weak Polyfunctional Basic Titrant.")
-a.plot_titration_curve()
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Polyprotic Acidic Analyte, Weak Polyfunctional Basic Titrant.")
+
+"""Base Analytes"""
+
+# Strong Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
+g = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[-6],
+              pkt_values=[-6],
+              strong_analyte=True,
+              strong_titrant=True,
+              title="Strong Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
+
+# Weak Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
+h = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[6],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Monofunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
+
+# Weak Difunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
+i = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[3, 9],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Difunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
+
+# Weak Trifunctional Basic Analyte, Strong Monoprotic Acidic Titrant.
+j = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[3, 6, 9],
+              pkt_values=[-6],
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Trifunctional Basic Analyte, Strong Monoprotic Acidic Titrant.")
+
+# Weak Monofunctional Basic Analyte, Weak Monoprotic Acidic Titrant.
+k = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[7],
+              pkt_values=[5],
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Monofunctional Basic Analyte, Weak Monoprotic Acidic Titrant.")
+
+# Weak Polyfunctional Base Analyte, Weak Polyprotic Acid Titrant.
+l = Titration(analyte_is_acidic=False,
+              titrant_is_acidic=True,
+              volume_analyte=25,
+              concentration_analyte=0.10,
+              concentration_titrant=0.10,
+              pka_values=[3, 4.5, 5, 6, 7.23, 8, 9, 10, 11, 12],
+              pkt_values=[3, 4.5, 5, 6, 7, 8, 9, 10, 11, 12],
+              strong_analyte=False,
+              strong_titrant=True,
+              title="Weak Polyfunctional Base Analyte, Weak Polyprotic Acid Titrant.")
+
+tests = [a, b, c, d, e, f, g, h, i, j, k, l]
+
+for test in tests:
+    test.plot_titration_curve()
+    # test.plot_alpha_curve()
