28bbd5fa663dac5090f77da319879f1a9f938ed4
Jackson Elowitt <jkelowitt@protonmail.com>
2020-12-23 14:33:39 -0800

    Changed tested example.
    Added titles to the curve and data creation.

--- single_test.py
+++ single_test.py
@@ -1,18 +1,17 @@
 from titration_class import Titration
 
 
-a = Titration(analyte_is_acidic=True,
-              titrant_is_acidic=False,
+# EDTA ion Analyte, HCl Acid Titrant.
+a = Titration(analyte_is_acidic=False,
               volume_analyte=25,
               concentration_analyte=0.10,
               concentration_titrant=0.10,
-              pka_values=[-6],
+              pka_values=[2.0, 2.7, 6.16, 10.26],
               pkt_values=[-6],
-              strong_analyte=True,
-              strong_titrant=True,
-              title="Strong Monoprotic Acidic Analyte, Strong Monoprotic Basic Titrant")
+              strong_analyte=False,
+              strong_titrant=True)
 
-a.plot_titration_curve()
-a.plot_alpha_curve()
-a.write_titration_data()
-a.write_alpha_data()
+a.plot_titration_curve("titration")
+a.plot_alpha_curve("titration")
+a.write_titration_data("alpha")
+a.write_alpha_data("alpha")
